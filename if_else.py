d0093e1646c1bfb4cc7f2499e0780f39b4cd0bcf
muhammad furqan javed <mfurqanjaved@gmail.com>
2023-07-07 04:37:27 +0500

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -15,14 +15,14 @@ Constraints
 import sys
 
 
-N = int(raw_input().strip())
+N = int(input().strip())
 
 if N % 2 != 0:
-    print "Weird"
+    print ("Weird")
 else:
     if N >= 2 and N <= 5:
-        print "Not Weird"
+        print ("Not Weird")
     elif N >= 6 and N <= 20:
-        print "Weird"
+        print ("Weird")
     elif N > 20:
         print ("Not Weird")
